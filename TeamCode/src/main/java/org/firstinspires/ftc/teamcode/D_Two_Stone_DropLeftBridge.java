c6ccc9fa1e1e2cd51387e3cdc322588bc043a3dd
codebuster11471 <codebusters11471@gmail.com>
2020-01-20 17:13:20 -0800

    Increasing distance under the bridge for drop off

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/D_Two_Stone_DropLeftBridge.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/D_Two_Stone_DropLeftBridge.java
@@ -146,14 +146,14 @@ public class D_Two_Stone_DropLeftBridge extends LinearOpMode {
                 intakeOperation(1); //Intake on
                 pidDriveCommand(39, -13,  -38, 0.3, 5);  //1st stone grab
                 pidDriveCommand(27, -12, 0, 0.6, 5);  //1st backup
-                pidDriveCommand(27, -56, 0, 0.75, 5);  //1st bridge
+                pidDriveCommand(27, -57, 0, 0.75, 5);  //1st bridge
                 intakeOperation(-1); //Intake out
                 pidDriveCommand(-1, -1,  -1, 0, 0.5);  //1st drop off
                 pidDriveCommand(23, 10, 0, 0.75, 5);  //2nd position
                 intakeOperation(1); //Intake on
-                pidDriveCommand(35, 10,  -30, 0.3, 5);  //2nd stone grab
+                pidDriveCommand(37, 12,  -30, 0.3, 5);  //2nd stone grab
                 pidDriveCommand(23, 5, 0, 0.6, 5);  //2nd backup
-                pidDriveCommand(27, -56, -10, 0.75, 5); //2nd bridge
+                pidDriveCommand(27, -57, -10, 0.75, 5); //2nd bridge
                 intakeOperation(-1); //Intake out
                 pidDriveCommand(-1, -1,  -1, 0, 0.5);  //2nd drop off
                 pidDriveCommand(26, -35, 0, 0.4, 5);  //Alliance park
@@ -164,14 +164,14 @@ public class D_Two_Stone_DropLeftBridge extends LinearOpMode {
                 intakeOperation(1); //Intake on
                 pidDriveCommand(39, -5,  -38, 0.3, 5);  //1st stone grab
                 pidDriveCommand(27, -4, 0, 0.6, 5);  //1st backup
-                pidDriveCommand(27, -56, 0, 0.75, 5);  //1st bridge
+                pidDriveCommand(27, -57, 0, 0.75, 5);  //1st bridge
                 intakeOperation(-1); //Intake out
                 pidDriveCommand(-1, -1,  -1, 0, 0.5);  //1st drop off
                 pidDriveCommand(23, 18, 0, 0.75, 5);  //2nd position
                 intakeOperation(1); //Intake on
                 pidDriveCommand(37, 20,  -30, 0.3, 5);  //2nd stone grab
                 pidDriveCommand(23, 5, 0, 0.6, 5);  //2nd backup
-                pidDriveCommand(27, -56, -10, 0.75, 5); //2nd bridge
+                pidDriveCommand(27, -57, -10, 0.75, 5); //2nd bridge
                 intakeOperation(-1); //Intake out
                 pidDriveCommand(-1, -1,  -1, 0, 0.5);  //2nd drop off
                 pidDriveCommand(26, -35, 0, 0.4, 5);  //Alliance park
@@ -182,14 +182,14 @@ public class D_Two_Stone_DropLeftBridge extends LinearOpMode {
                 intakeOperation(1); //Intake on
                 pidDriveCommand(39, 3,  -38, 0.3, 5);  //1st stone grab
                 pidDriveCommand(27, 4, 0, 0.6, 5);  //1st backup
-                pidDriveCommand(27, -56, 0, 0.75, 5);  //1st bridge
+                pidDriveCommand(27, -57, 0, 0.75, 5);  //1st bridge
                 intakeOperation(-1); //Intake out
                 pidDriveCommand(-1, -1,  -1, 0, 0.5);  //1st drop off
                 pidDriveCommand(23, 26, 0, 0.75, 5);  //2nd position
                 intakeOperation(1); //Intake on
                 pidDriveCommand(35, 26,  -30, 0.3, 5);  //2nd stone grab
                 pidDriveCommand(23, 5, 0, 0.6, 5);  //2nd backup
-                pidDriveCommand(27, -56, -10, 0.75, 5); //2nd bridge
+                pidDriveCommand(27, -57, -10, 0.75, 5); //2nd bridge
                 intakeOperation(-1); //Intake out
                 pidDriveCommand(-1, -1,  -1, 0, 0.5);  //2nd drop off
                 pidDriveCommand(26, -35, 0, 0.4, 5);  //Alliance park
